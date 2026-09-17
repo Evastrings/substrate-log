46745d1947b02628ee1245439425f52199e5a05e
Evastrings <elijahakandetea@gmail.com>
2026-09-17 16:22:34 -0500

    hello_world/count_characters

---
+++ introduction-to-algorithm-and-machine-learning/hello_world/count_characters.py
@@ -0,0 +1,23 @@
+"""Count the number of each character in a string"""
+
+def count_characters(string):
+    """
+    Counts the number of each character in a string and returns the count in a dictionary.
+    """
+
+    string = str(string)
+    string = string.lower()
+    result = {}
+
+    for element in string:
+        counter = 0
+        for char in string:
+            if element == char:
+                counter += 1
+
+        result.update({element: counter})
+
+    return result
+
+test = count_characters('A cat!!!')
+print(test)
