46b7a1c725da9bdd90d2f4eca3026ec7b9180861
Evastrings <elijahakandetea@gmail.com>
2026-09-17 22:08:08 -0500

    cleaned is_prime

--- introduction-to-algorithm-and-machine-learning/hello_world/is_prime.py
+++ introduction-to-algorithm-and-machine-learning/hello_world/is_prime.py
@@ -7,26 +7,10 @@ def is_prime(N):
     """
     if N > 1:
         for n in range(2, (N//2)+1):
+            # or (N**2) + 1
             if N % n == 0:
                 return False
         return True
     else:
-        return 'Enter numbers greater than 1'
+        return False
     
-
-# print(5//2)
-# print(27//10)
-# print(99//10)
-# print(-33//10)
-
-print(is_prime(4))
-print(is_prime(51))
-
-print(is_prime(1001))
-print(is_prime(1))
-print(is_prime(51))
-
-print(is_prime(1009))
-print(is_prime(1297))
-print(is_prime(1000081))
-# print(type(5) == 'int')
