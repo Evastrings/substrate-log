8a80600da977401bfdb3fc86bcc5af91d07f6583
Evastrings <elijahakandetea@gmail.com>
2026-09-19 00:44:27 -0500

    added input validation to binary_to_decimal function

--- introduction-to-algorithm-and-machine-learning/hello_world/binary_to_decimal.py
+++ introduction-to-algorithm-and-machine-learning/hello_world/binary_to_decimal.py
@@ -4,32 +4,14 @@ def binary_to_decimal(string):
     """
     Take a binary representation of a number and return the decimal representation.
     """
+
+    for digits in string:
+        if digits not in ("0", "1"):
+            return 'Invalid binary number'
     result = 0
 
     for index, digit in enumerate(string):
-        exponent = len(string) - index -1
+        exponent = len(string) - index - 1
         result += int(digit) * (2 ** exponent)
-        index += 1
 
     return result
-
-binn = '11010'
-
-b1 = '1'
-
-b2 = '111'
-
-b3 = '10'
-
-b4 = '11'
-
-b5 = '100'
-
-dinn = binary_to_decimal(binn)
-print(f" 11010 in decimal is {dinn}")
-
-print(binary_to_decimal(b1))
-test = [b1, b2, b3, b4, b5]
-for i in test:
-    print(f"The value of {i} in decimal is {binary_to_decimal(i)}")
-
