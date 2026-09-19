e7e5f080cfc7bf5bf9425b01a9553bd040e9cc93
Evastrings <elijahakandetea@gmail.com>
2026-09-19 13:59:37 -0500

    hello_world/decimal_to_hexadecimal

--- introduction-to-algorithm-and-machine-learning/hello_world/decimal_to_binary.py
+++ introduction-to-algorithm-and-machine-learning/hello_world/decimal_to_binary.py
@@ -13,23 +13,23 @@ def decimal_to_binary(string):
             return 'Invalid decimal number'
 
     string = int(string)
-    binary_num = ''
+    reversed_binary = ''
     result = ''
 
     if string == 0:
         return str(string)
     
     while string != 0:
-        binary_num += str(string % 2)
+        reversed_binary += str(string % 2)
         string //= 2
 
-    for i in range(1, len(binary_num) + 1):
-        result += binary_num[-i]
+    for i in range(1, len(reversed_binary) + 1):
+        result += reversed_binary[-i]
 
     return result
 
 
-test = [0, 1, 2, 3, 4, 5, 8, 16, 26, 32, '64', 'a3', 56]
+test = ['0', '1', '2', '3', '4', '5', '8', '16', '26', '32', '64', 'a3', 56]
 tests = [2]
 
 for nu in test:
---
+++ introduction-to-algorithm-and-machine-learning/hello_world/decimal_to_hexadecimal.py
@@ -0,0 +1,52 @@
+"""Convert decimal to hexadecimal"""
+
+def decimal_to_hexadecimal(string):
+    """
+    Take a decimal representation of a number and return the hexadecimal representation
+    """
+    reversed_hex_dict = {
+        '0': '0',
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9',
+        '10': 'a',
+        '11': 'b',
+        '12': 'c',
+        '13': 'd',
+        '14': 'e',
+        '15': 'f'
+    }
+
+    if not isinstance(string, str):
+        return 'Enter a String'
+    
+    for digit in string:
+        if digit not in reversed_hex_dict:
+            return 'Invalid decimal number'
+
+    if string == '0':
+        return reversed_hex_dict[string]
+
+    string = int(string)
+    result = ''
+    while string != 0:
+        result = reversed_hex_dict[str(string % 16)] + result
+        string //= 16
+
+    return result
+
+d = '4770'
+h = decimal_to_hexadecimal(d)
+
+print(f" {d} converted to hexadecimal is {h}")
+
+test = ['11259375', '974010', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', 16, '16', '31', '32', '3A4']
+
+for t in test:
+    print(f"{t} in hexadecimal is {decimal_to_hexadecimal(t)}")
