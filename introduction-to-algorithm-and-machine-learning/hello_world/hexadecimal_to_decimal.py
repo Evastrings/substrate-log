522d6fef7f2bbcfffb0190df033d13a63256b22a
Evastrings <elijahakandetea@gmail.com>
2026-09-18 22:52:01 -0500

    cleaner hex to dec function

--- introduction-to-algorithm-and-machine-learning/hello_world/hexadecimal_to_decimal.py
+++ introduction-to-algorithm-and-machine-learning/hello_world/hexadecimal_to_decimal.py
@@ -4,10 +4,22 @@ def hexadecimal_to_decimal(string):
     """
     Take a hexadecimal representation of a number and return the decimal representation
     """
+    
 
     string = string.lower()
     result = 0
+
     hex_dict = {
+        '0': 0,
+        '1': 1,
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
         'a': 10,
         'b': 11,
         'c': 12,
@@ -17,16 +29,16 @@ def hexadecimal_to_decimal(string):
     }
 
     for index, digit in enumerate(string):
-        exponent = len(string) - index - 1
-
         if digit not in hex_dict:
-            result += int(digit) * (16 ** exponent)
-
-        else:
-            result += hex_dict[digit] * (16 ** exponent)
+            return 'Invalid Hexadecimal Number'
+        
+        exponent = len(string) - index - 1
+        result += hex_dict[digit] * (16 ** exponent)
 
     return result
 
 hex_c = '7CF'
 dec_c = hexadecimal_to_decimal(hex_c)
 print(f"{hex_c} to decimal is {dec_c}")
+
+print(hexadecimal_to_decimal('ABCDEF'))
