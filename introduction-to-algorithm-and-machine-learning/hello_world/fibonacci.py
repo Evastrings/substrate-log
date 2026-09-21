6c6b7e92a9411a8d9ef69b47d9dfbc9a6488e17e
Evastrings <elijahakandetea@gmail.com>
2026-09-20 23:01:07 -0500

    hello_world/fibonacci --recursive function

--- introduction-to-algorithm-and-machine-learning/hello_world/fibonacci.py
+++ introduction-to-algorithm-and-machine-learning/hello_world/fibonacci.py
@@ -22,8 +22,31 @@ def fibonacci(n):
 
     return terms
 
-print(fibonacci(13))
-for i in range(-4, 13):
-    print(fibonacci(i))
 
+def fibonacci_nth(n):
+    """
+    Returns nth term of the fibonacci sequence
+    """
+    if not isinstance(n, int):
+        return 'Invalid Integer'
     
+    if n < 1:
+        return 'n must be at least 1'
+
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+
+    return fibonacci_nth(n - 1) + fibonacci_nth(n - 2)
+
+if __name__ == '__main__':
+
+    print(fibonacci(13))
+    for i in range(-4, 13):
+        print(fibonacci(i))
+
+    test = [1, -2, 3, 7, 8, 13, 0, 1, 2, 3, 4, 5, 6, 7, 8, 'r,', '20', -56, '13']
+    print("=== Testing the nth term function ===")
+    for i in test:
+        print(f"{i}th term in the fibonacci sequence is {fibonacci_nth(i)}")
